5b23a961161189f85a3b2698714227bd435789b8
inaralmeida <inara.ralmeida@gmail.com>
2023-06-05 14:52:15 -0300

    fix: images

---
+++ src/assets/logos/images.ts
@@ -0,0 +1,27 @@
+import blogr from './blogr.svg'
+import coffeeroasters from './coffeeroasters.svg'
+import creative from './creative.svg'
+import crowdfund from './crowdfund.svg'
+import maker from './maker.svg'
+import mastercraft from './mastercraft.svg'
+import officelite from './officelite.svg'
+import pod from './pod.svg'
+import pomodoro from './pomodoro.svg'
+import scoot from './scoot.svg'
+import typemaster from './typemaster.svg'
+import vector from './vector.svg'
+
+export default {
+  blogr,
+  coffeeroasters,
+  creative,
+  crowdfund,
+  maker,
+  mastercraft,
+  officelite,
+  pod,
+  pomodoro,
+  scoot,
+  typemaster,
+  vector
+}
