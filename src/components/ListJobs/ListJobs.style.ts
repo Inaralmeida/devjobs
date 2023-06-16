9a0c21348ff4ab39fe4941c858eff4b96a28ed1d
inaralmeida <inara.ralmeida@gmail.com>
2023-06-16 01:11:24 -0300

    fix: gap

--- src/components/ListJobs/ListJobs.style.ts
+++ src/components/ListJobs/ListJobs.style.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 export const StyleListJobs = styled.ul`
   display: flex;
   flex-wrap: wrap;
-  gap: 20px;
+  gap: 60px;
   padding: 0;
   justify-content: space-between;
+  margin-top: 50px;
 `
