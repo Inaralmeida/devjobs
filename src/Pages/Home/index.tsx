f46182bfda32edb1078a79794ca9a57999b0a7d6
inaralmeida <inara.ralmeida@gmail.com>
2023-06-16 01:03:33 -0300

    fix: api

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react'
+import ListJobs from '../../components/ListJobs'
+import { get } from '../../API/api'
 import BarSearch from '../../components/BarSearch'
 
 const Home = () => {
+  const [list, setList] = useState()
+
+  const handleGetJobs = async () => {
+    const data = await get()
+    setList(data)
+  }
+  useEffect(() => {
+    handleGetJobs()
+  }, [])
   return (
     <div className="container">
       <BarSearch />
+      {list && <ListJobs listJobs={list} />}
     </div>
   )
 }
