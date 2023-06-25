e4acb07816c58aecf316fc1408a5581e4c8b1474
inaralmeida <inara.ralmeida@gmail.com>
2023-06-25 19:10:04 -0300

    fix: header job

--- src/Pages/Jobs/index.tsx
+++ src/Pages/Jobs/index.tsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { get } from '../../API/api'
+import HeaderJob from '../../components/HeaderJob'
+import { images } from '../../assets/logos/images'
+
+type JobProps = {
+  id: number
+  company: string
+  logo: string
+  logoBackground: string
+  position: string
+  postedAt: string
+  contract: string
+  location: string
+  website: string
+  apply: string
+  description: string
+  requirements: {
+    content: string
+    items: string[]
+  }
+  role: {
+    content: string
+    items: string[]
+  }
+}
 
 const Jobs = () => {
   const params = useParams()
-  console.log(params)
-  return <div> page Jobs</div>
+  const [job, setJob] = useState<JobProps>()
+
+  const handleGetJob = async (params: any) => {
+    const data: JobProps = await get(true, Number(params.id))
+    setJob(data)
+  }
+
+  useEffect(() => {
+    handleGetJob(params)
+  }, [])
+
+  return (
+    <div className="container">
+      <HeaderJob
+        company={job?.company}
+        image={images[job ? job?.company.toLowerCase() : 0]}
+        website={job?.website}
+        colorbg={job?.logoBackground}
+      />
+    </div>
+  )
 }
 
 export default Jobs
