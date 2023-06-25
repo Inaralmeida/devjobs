021c6145af5be2bd88c382cb5911def5f355fab5
inaralmeida <inara.ralmeida@gmail.com>
2023-06-25 19:07:51 -0300

    feat: get job by id

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,9 +1,21 @@
 import axios from 'axios'
 
-export const get = async () => {
-  const response = await axios.get('src/API/data.json', {
-    headers: { Accept: 'application/json' }
-  })
+export const get = async (filter = false, id = 0) => {
 
-  return response.data
+  const response = await axios.get('/src/API/data.json', {
+    headers: {
+      Accept: 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+  const data = response.data
+  if (!filter) {
+    return data
+  } else {
+    const [job] = await data.filter((job: any) => job.id === id)
+
+    return job
+  }
 }
+
+
