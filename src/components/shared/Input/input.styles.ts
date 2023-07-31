fa190dff33d2f2f728a5dcfc23542d98e85ab592
inaralmeida <inara.ralmeida@gmail.com>
2023-07-30 23:44:23 -0300

    fix: responsiveness inputs

--- src/components/shared/Input/input.styles.ts
+++ src/components/shared/Input/input.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StylesInput = styled.div`
   width: 100%;
@@ -7,23 +7,29 @@ export const StylesInput = styled.div`
   justify-content: flex-start;
   gap: 10px;
   padding: 20px 15px;
-  background-color: ${p => p.theme.gray100};
-  
-  > input{
+  background-color: ${(p) => p.theme.gray100};
+
+  > input {
     background-color: transparent;
     height: 50px;
     width: 100%;
     outline: none;
-    border:none;
+    border: none;
     font-style: normal;
     font-weight: 400;
     font-size: 16px;
     line-height: 20px;
-    color: ${p => p.theme.black};
+    color: ${(p) => p.theme.black};
 
-    ::placeholder{
+    ::placeholder {
       opacity: 0.7;
     }
   }
- 
+
+  @media screen and (max-width: 425px) {
+    > input {
+      height: 30px;
+    }
+    padding: 5px 5px;
+  }
 `
